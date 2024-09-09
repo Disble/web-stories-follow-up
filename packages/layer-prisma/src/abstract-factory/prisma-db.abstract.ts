4c62fd215a6a5b05ffd8222be97f5f38f8fed75b
Disble <decoder@hotmail.es>
2024-09-09 12:31:23 -0500

    style: :bulb: remove comment

--- packages/layer-prisma/src/abstract-factory/prisma-db.abstract.ts
+++ packages/layer-prisma/src/abstract-factory/prisma-db.abstract.ts
@@ -3,7 +3,6 @@ import type { ModelFactory } from "#factory/model-factory";
 export abstract class PrismaDB {
   public constructor(
     protected readonly connect: ModelFactory["connect"],
-    // TODO: TOCA PROBAR SI FUNCIONA
     protected readonly db: ModelFactory
   ) {}
 }
