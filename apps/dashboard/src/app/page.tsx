b56fa487954ad15cfed8169c2d869521eeecba6b
Disble <decoder@hotmail.es>
2024-09-10 16:44:16 -0500

    refactor(web): :truck: change path routes system

--- apps/dashboard/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { PATH_AUTH } from "#routes/index";
-import { redirect } from "next/navigation";
-
-export default async function IndexPage() {
-  redirect(PATH_AUTH.signin);
-}
--- apps/dashboard/src/routes/index.ts
+++ apps/dashboard/src/routes/index.ts
@@ -1,35 +1,36 @@
 function path(root: string, ...sublink: string[]) {
-  return `${root}${sublink.join("")}`;
+  const joinedSublink = sublink.join("/");
+  return root === "/" ? `/${joinedSublink}` : `${root}/${joinedSublink}`;
 }
 
-const ROOTS_DASHBOARD = "/dashboard";
+const ROOTS_DASHBOARD = "/";
 const ROOTS_AUTH = "/auth";
 
 export const PATH_DASHBOARD = {
   root: ROOTS_DASHBOARD,
   default: path(ROOTS_DASHBOARD),
   users: {
-    list: path(ROOTS_DASHBOARD, "/users"),
-    create: path(ROOTS_DASHBOARD, "/users", "/create"),
-    update: path(ROOTS_DASHBOARD, "/users", "/update"),
+    list: path(ROOTS_DASHBOARD, "users"),
+    create: path(ROOTS_DASHBOARD, "users", "create"),
+    update: path(ROOTS_DASHBOARD, "users", "update"),
   },
   novels: {
-    list: path(ROOTS_DASHBOARD, "/novels"),
-    create: path(ROOTS_DASHBOARD, "/novels", "/create"),
-    update: path(ROOTS_DASHBOARD, "/novels", "/update"),
+    list: path(ROOTS_DASHBOARD, "novels"),
+    create: path(ROOTS_DASHBOARD, "novels", "create"),
+    update: path(ROOTS_DASHBOARD, "novels", "update"),
   },
   authors: {
-    list: path(ROOTS_DASHBOARD, "/authors"),
-    create: path(ROOTS_DASHBOARD, "/authors", "/create"),
-    update: path(ROOTS_DASHBOARD, "/authors", "/update"),
+    list: path(ROOTS_DASHBOARD, "authors"),
+    create: path(ROOTS_DASHBOARD, "authors", "create"),
+    update: path(ROOTS_DASHBOARD, "authors", "update"),
   },
   publications: {
-    list: path(ROOTS_DASHBOARD, "/publications"),
-    create: path(ROOTS_DASHBOARD, "/publications", "/create"),
-    update: path(ROOTS_DASHBOARD, "/publications", "/update"),
+    list: path(ROOTS_DASHBOARD, "publications"),
+    create: path(ROOTS_DASHBOARD, "publications", "create"),
+    update: path(ROOTS_DASHBOARD, "publications", "update"),
   },
   settings: {
-    list: path(ROOTS_DASHBOARD, "/settings"),
+    list: path(ROOTS_DASHBOARD, "settings"),
   },
 } as const;
 
